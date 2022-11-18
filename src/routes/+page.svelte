e4824b95174701e4dd39d27dbb63d5de4626977c
Finnvb <finnvb@gmail.com>
2022-11-18 13:00:52 +0100

    global styles added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-    import Clock from 'ilojo-bar-components/components/Clock.svelte'
+	import Clock from 'ilojo-bar-components/components/Clock.svelte';
+    import '../styles/global.css'
 </script>
-
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<Clock/>
+<Clock />
