30a2f452cab072027c8ea1bd59e0d3066ad080c2
Finnvb <finnvb@gmail.com>
2022-11-22 12:26:29 +0100

    Data Fetched

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import PageTransition from '$lib/components/PageTransition.svelte';
+    
+    /** @type {import('./$types').LayoutData} */
+    export let data;
+        import '../styles/global.css'
+    
+    </script>
+    
+    
+    <PageTransition pathname={data.pathname}>
+    <slot></slot>
+    
+    </PageTransition>
+    
+   
---
+++ src/routes/overview/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	// import Header from '../../lib/components/Header.svelte';
+	// import Footer from '../../lib/components/Footer.svelte';
+	// import Button from '$lib/components/Button.svelte';
+	// import ScrollToTopButton from '../../lib/components/ScrollToTopButton.svelte';
+	import * as prismicH from '@prismicio/helpers';
+	export let data;
+
+	const { document } = data;
+
+	console.log(document);
+
+
+
+
+
+</script>
+
+<!-- <Header /> -->
+
+<main>
+	{#each data.document as data}
+		<section>
+			<div class="image-container">
+				<img class="storyimage" src={data.data.storyimages[0].image1.url} alt="test" />
+			</div>
+
+			<div class="window">
+				<img class="window" src="window.svg" alt="window" />
+				<img class="windowLeft" src="windowLeft.svg" alt="window" />
+				<img class="windowRight" src="windowRight.svg" alt="window" />
+				<div class="test" />
+			</div>
+
+			<h2>{@html prismicH.asHTML(data.data.storytitle)}</h2>
+
+			<!-- <Button source={"goldplate.png"} link={data.uid} linktext={'Discover'} height={'4rem'} color={("#6c5334")} /> -->
+		</section>
+	{/each}
+
+	<!-- <ScrollToTopButton uid="overview" /> -->
+	
+</main>
